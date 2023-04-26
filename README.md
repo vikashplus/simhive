c63e6e72f6926898ddc18319aa83c65c9691fe83
Vikash Kumar <vikashplus@gmail.com>
2023-04-26 16:10:41 -0400

    MODELS, TESTS: Adding a link to all the models and unit tests

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# simhive
-A collection of MuJoCo simulation models
+# SimHive
+SimHive is the physics simulation library of [RoboHive](https://sites.google.com/view/robohive). It consists of a diverse collection of MuJoCo simulation models designed with care for physical realism.
+
+# Usage instructions
+To benefit from `SimHive`, we strongly encourage our users to -
+- use *SimHive* via [RoboHive](https://sites.google.com/view/robohive)
+- If direct access is required, use [git submodule](https://git-scm.com/book/en/v2/Git-Tools-Submodules) link to SimHive (or its subcomponents), instead of copying it over. Submodules allow you to keep *SimHive* as a fully functional repository as a subdirectory to your git repository. Amongst numerous benefits, the ability to easily pull upstream changes from SimHive tops the chart. Its a easy as -
+``` bash
+# Add entire SimHive as submodule
+git submodule add https://github.com/vikashplus/simhive.git <simhive_folder_path>
+
+# Add selected modules (franka_sim) in your project
+git submodule add https://github.com/vikashplus/franka_sim.git
+```
+The latter example shows how to selectively add modules. See [gitmodules](.gitmodules) files for individual links.
---
+++ tests/test_simhive.py
@@ -0,0 +1,148 @@
+import unittest
+import os
+import glob
+
+try:
+    import mujoco_py
+    from mujoco_py import load_model_from_path, MjSim, load_model_from_xml
+except ImportError as e:
+    raise ImportError("(HINT: you need to install mujoco_py, and also perform the setup instructions here: https://github.com/openai/mujoco-py/.)")
+
+
+class TestSims(unittest.TestCase):
+
+    def get_sim(self, model_path:str=None, model_xmlstr=None):
+        """
+        Get sim using model_path or model_xmlstr.
+        """
+        if model_path:
+            if model_path.startswith("/"):
+                fullpath = model_path
+            else:
+                fullpath = os.path.join(os.path.dirname(__file__), model_path)
+            if not os.path.exists(fullpath):
+                raise IOError("File %s does not exist" % fullpath)
+            model = load_model_from_path(fullpath)
+        elif model_xmlstr:
+            model = load_model_from_xml(model_xmlstr)
+        else:
+            raise TypeError("Both model_path and model_xmlstr can't be None")
+
+        return MjSim(model)
+
+
+    def test_adroit_sim(self):
+        model_paths = glob.glob("../simhive/Adroit/*.xml")
+        print("\nSimHive: Tesing Adroit")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_dmanus_sim(self):
+        model_paths = glob.glob("../simhive/dmanus_sim/*.xml")
+        print("\nSimHive: Tesing D'Manus")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_fetch_sim(self):
+        model_paths = glob.glob("../simhive/fetch_sim/*.xml")
+        print("\nSimHive: Tesing Fetch")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_franka_sim(self):
+        model_paths = glob.glob("../simhive/franka_sim/*.xml")
+        print("\nSimHive: Tesing Franka")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_furniture_sim(self):
+        model_paths = glob.glob("../simhive/furniture_sim/*.xml")
+        print("\nSimHive: Tesing Furniture Sim")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_myo_sim(self):
+        model_paths = glob.glob("../simhive/myo_sim/*/*.xml")
+
+        print("\nSimHive: Tesing Myo")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_object_sim(self):
+        model_paths = glob.glob("../simhive/object_sim/*.xml")
+
+        print("\nSimHive: Tesing Objects")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_robel_sim(self):
+        model_paths = glob.glob("../simhive/robel_sim/d*/*.xml")
+
+        print("\nSimHive: Tesing ROBEL")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_robotiq_sim(self):
+        model_paths = glob.glob("../simhive/robotiq_sim/*.xml")
+
+        print("\nSimHive: Tesing RobotiQ")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_sawyer_sim(self):
+        model_paths = glob.glob("../simhive/sawyer_sim/*.xml")
+
+        print("\nSimHive: Tesing Sawyer")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_scene_sim(self):
+        model_paths = glob.glob("../simhive/scene_sim/*.xml")
+
+        print("\nSimHive: Tesing Scene")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_trifinger_sim(self):
+        model_paths = glob.glob("../simhive/trifinger_sim/trifinger.xml")
+
+        print("\nSimHive: Tesing Trifinger")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+    def test_ycb_sim(self):
+        model_paths = glob.glob("../simhive/YCB_sim/*.xml")
+
+        print("\nSimHive: Tesing YCB")
+        for model_path in model_paths:
+            print("\t{}".format(model_path))
+            self.get_sim(model_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
